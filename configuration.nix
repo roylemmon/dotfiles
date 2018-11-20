68e5b76f0c1422704c8c5df335378ab5d885933d
Roy Lemmon <roy@roylemmon.com>
2018-11-20 15:55:40 +0000

    Add files via upload

---
+++ configuration.nix
@@ -0,0 +1,145 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "parton"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  fileSystems."/mnt/nas" = {
+    device = "192.168.1.17:/Multimedia";
+    fsType = "nfs";
+    options = ["x-systemd.automount" "noauto"];
+  };
+
+
+  # Select internationalisation properties.
+  # i18n = {
+  #   consoleFont = "Lat2-Terminus16";
+  #   consoleKeyMap = "us";
+  #   defaultLocale = "en_US.UTF-8";
+  # };
+
+  # Set your time zone.
+  time.timeZone = "Europe/London";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget
+    git
+    vim
+    dmenu
+    emacs
+    evince
+    firefox
+    rxvt_unicode
+    gcc
+    poppler
+    gnumake
+    automake
+    autoconf
+    binutils
+    stack
+    haskellPackages.xmobar
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+  # services.xserver.libinput.enable = true;
+
+  # Enable the KDE Desktop Environment.
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
+
+  services.xserver = {
+    enable = true;
+    layout = "gb";
+    xkbOptions = "ctrl:nocaps";
+    desktopManager.default = "none";
+    desktopManager.xterm.enable = false;
+    displayManager.slim.defaultUser = "rlemmon";
+
+    windowManager.default = "xmonad";
+    windowManager.xmonad = {
+      enable = true;
+      enableContribAndExtras = true;
+      extraPackages = haskellPackages : [
+      haskellPackages.xmobar
+      haskellPackages.xmonad-contrib
+      haskellPackages.xmonad-extras
+      ];
+    };
+    
+    libinput = {
+    enable = true;
+    naturalScrolling = true;
+    };
+  };    
+
+fonts.fonts = with pkgs; [
+  ubuntu_font_family
+  dejavu_fonts
+  powerline-fonts
+];
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.extraUsers.rlemmon = {
+    createHome = true;
+    extraGroups = ["wheel" "networkmanager"];
+    group = "users";
+    home = "/home/rlemmon";
+    isNormalUser = true;
+    uid = 1000;
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "18.09"; # Did you read the comment?
+
+}
